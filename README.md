88b06c7d93966c1338bfc0692f1a63ea6832f39f
Brian Leung <brianleung1017@gmail.com>
2022-12-16 00:19:54 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# lihkg-hot-thread-crawler
+# LIHKG Hot Thread Crawler
+
+This project is to fetch the hot threads (now, daily, weekly) hourly via GitHub Actions. All JSON file are stored in gh-pages branch.
---
+++ crawlHotThread.py
@@ -0,0 +1,63 @@
+import os
+import json
+import sys
+from datetime import datetime, tzinfo
+import pytz
+
+from seleniumwire import webdriver  # Import from seleniumwire
+from selenium.webdriver.chrome.options import Options
+from seleniumwire.utils import decode
+
+type = sys.argv[1]
+if type not in ['now', 'daily', 'weekly']:
+    raise ValueError("Please include [now], [daily] or [weekly] as argument")
+
+print(f"Fetching type: {type}")
+
+chrome_options = Options()
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--incognito")
+# fake user agent
+ua = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"
+chrome_options.add_argument("user-agent={}".format(ua))
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome(options=chrome_options)
+
+url = f'https://lihkg.com/category/2?type={type}'
+
+print(url)
+# Go to the Google home page
+driver.get(url)
+
+# Access requests via the `requests` attribute
+for request in driver.requests:
+    if "/thread/hot" in request.url and request.response:
+        # Get current datetime in UTC
+        utc_now_dt = datetime.now(tz=pytz.UTC).strftime("%Y%m%d_%H%M%S")
+        # decode an encoded response body
+        decoded_body = decode(
+            request.response.body,
+            request.response.headers.get('Content-Encoding', 'identity'))
+
+        # decode bytes object to a string
+        json_str = decoded_body.decode('utf-8')
+
+        # convert json string to dictionary
+        data = json.loads(json_str)
+
+        print(request.url, request.response.status_code,
+              request.response.headers['Content-Type'],
+              request.response.headers.get('Content-Encoding', 'identity'))
+
+        file_name = os.path.join(type, f'{utc_now_dt}.json')
+        dirname = os.path.dirname(file_name)
+
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        if not os.path.isfile(file_name):
+            with open(file_name, 'w') as f:
+                f.write(json.dumps(data, ensure_ascii=False))
